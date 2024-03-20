b7b11e355edff04bc073a84ea75a4ad388928cfa
siva mani <sivamavi212@gmail.com>
2024-03-20 18:13:07 +0530

    node just

--- server/index.js
+++ server/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const Axios = require("axios");
 const helmet = require("helmet");
-const path = require("path");
+
 const bodyParser = require("body-parser");
 const connection = require("./controllers/Database");
 const codeRouter = require("./Routes/CodeRouter");
 const databaseRouter = require("./Routes/databaseRoutes");
-const buildPath = path.join(__dirname, "build");
 
-// Close MySQL connection
 
 const app = express();
 const PORT = 8000;
@@ -17,10 +15,7 @@ const PORT = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(express.static(buildPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
-});
+
 
 app.use("/code", codeRouter);
 app.use("/database", databaseRouter);
